06160a992ddf8abd02690e0933e36ee305f9ddf2
pittinic <nicolas.pittion@icloud.com>
2020-09-06 00:05:53 +0200

    fix(ssl-tls): remove super call in WebSecurityConfigurerAdapter
    said call prevented any further configuration and hence https redirection and permitAll

--- ssl-tls/src/main/kotlin/com/gnirps/tls/config/SslWebSecurityConfigurerAdapter.kt
+++ ssl-tls/src/main/kotlin/com/gnirps/tls/config/SslWebSecurityConfigurerAdapter.kt
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 class SslWebSecurityConfigurerAdapter : WebSecurityConfigurerAdapter() {
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
-        super.configure(http)
         http
                 .requiresChannel().anyRequest().requiresSecure()
                 .and().authorizeRequests().antMatchers("/**").permitAll()
