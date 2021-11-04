b419aadb887ca0337f8acccdc482f78cac309013
JeremyV <jeremy.vain@gmail.com>
2021-11-04 08:22:37 +0100

    refactor(json validator)/GNIR-17: remove class encapsulation

--- utils/src/main/kotlin/com/gnirps/utils/JsonValidator.kt
+++ utils/src/main/kotlin/com/gnirps/utils/JsonValidator.kt
@@ -3,14 +3,16 @@ package com.gnirps.utils
 import com.fasterxml.jackson.databind.ObjectMapper
 import java.io.IOException
 
-class JsonValidator {
-    fun String.isValidJson(): Boolean {
-        return try {
-            val mapper = ObjectMapper()
-            mapper.readTree(this)
-            true
-        } catch (e: IOException) {
-            false
-        }
+/**
+ * Whether a string is a valid JSON or not
+ * @return [Boolean]
+ */
+fun String.isValidJson(): Boolean {
+    return try {
+        val mapper = ObjectMapper()
+        mapper.readTree(this)
+        true
+    } catch (e: IOException) {
+        false
     }
 }
