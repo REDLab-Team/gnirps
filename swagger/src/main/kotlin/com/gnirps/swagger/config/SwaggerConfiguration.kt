7f739d7496ac748f40a46012b4ac45559c3390a4
JeremyV <jeremy.vain@gmail.com>
2025-05-15 17:45:54 +0200

    feat(swagger)/GNIR-33: takes into account "contact" and "security schemes" properties

--- swagger/src/main/kotlin/com/gnirps/swagger/config/SwaggerConfiguration.kt
+++ swagger/src/main/kotlin/com/gnirps/swagger/config/SwaggerConfiguration.kt
@@ -1,9 +1,12 @@
 package com.gnirps.swagger.config
 
 import com.gnirps.swagger.config.properties.SwaggerProperties
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.info.License
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springdoc.core.GroupedOpenApi
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Bean
@@ -20,16 +23,42 @@ class SwaggerConfiguration(val swaggerProperties: SwaggerProperties) {
             .pathsToMatch(swaggerProperties.api.exposedEndpoints.joinToString("|"))
             .build()
     }
+
     @Bean
     fun gnirpsOpenAPI(): OpenAPI {
-        return OpenAPI()
+        val openAPI = OpenAPI()
             .info(
-                Info().title(
-                    swaggerProperties.api.title
-                )
+                Info()
+                    .title(swaggerProperties.api.title)
+                    .contact(
+                        Contact()
+                            .name(swaggerProperties.maintainer.name)
+                            .email(swaggerProperties.maintainer.email)
+                            .url(swaggerProperties.maintainer.website)
+                    )
                     .description(swaggerProperties.api.description)
                     .version(swaggerProperties.api.version)
                     .license(License().name(swaggerProperties.api.license).url(swaggerProperties.api.licenseUrl))
             )
+
+        if (swaggerProperties.securitySchemes.isNotEmpty()) {
+            val components = Components()
+
+            swaggerProperties.securitySchemes.forEach { (name, config) ->
+                val scheme = SecurityScheme().type(config.type)
+                    .also {
+                        config.scheme?.let { s -> it.scheme = s }
+                        config.bearerFormat?.let { bf -> it.bearerFormat = bf }
+                        config.`in`?.let { i -> it.`in` = i }
+                        config.name?.let { n -> it.name = n }
+                    }
+
+                components.addSecuritySchemes(name, scheme)
+            }
+
+            openAPI.components(components)
+        }
+
+        return openAPI
     }
 }
